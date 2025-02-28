ed33d56ec09b35430bd95c9d5c195f1aed834a8e
Trilok <tiosuthar@gmail.com>
2025-02-28 10:38:05 +0530

    work on home page

--- src/app/(frontend)/components/ui/hero-parallax.tsx
+++ src/app/(frontend)/components/ui/hero-parallax.tsx
@@ -133,7 +133,7 @@ export const ProductCard = ({
         y: -20,
       }}
       key={product.title}
-      className="group/product h-96 w-[30rem] relative flex-shrink-0"
+      className="group/product  h-96 w-[30rem] relative flex-shrink-0"
     >
       <Link
         href={product.link}
--- src/heros/HighImpact/index.tsx
+++ src/heros/HighImpact/index.tsx
@@ -1,9 +1,7 @@
 'use client'
 import { useHeaderTheme } from '@/providers/HeaderTheme'
 import React, { useEffect } from 'react'
-
 import type { Page } from '@/payload-types'
-
 import { CMSLink } from '@/components/Link'
 import { Media } from '@/components/Media'
 import RichText from '@/components/RichText'
