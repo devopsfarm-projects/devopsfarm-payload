04ea728c0f769060f33d621a7dc30fc0ad7fc9ea
Trilok <tiosuthar@gmail.com>
2025-04-02 23:56:50 +0530

    update quiz

--- src/app/(frontend)/api/data/route.ts
+++ src/app/(frontend)/api/data/route.ts
@@ -1,19 +1,19 @@
-import { NextResponse } from "next/server";
-import { getPayload } from "payload";
-import config from "@payload-config";
+import { NextResponse } from 'next/server'
+import { getPayload } from 'payload'
+import config from '@payload-config'
 
 export async function GET(req: Request) {
   try {
-    const { searchParams } = new URL(req.url);
-    const queryParam = searchParams.get("query");
+    const { searchParams } = new URL(req.url)
+    const queryParam = searchParams.get('query')
 
-    const query = queryParam ? JSON.parse(queryParam) : {}; 
+    const query = queryParam ? JSON.parse(queryParam) : {}
 
-    const payload = await getPayload({ config });
-    const data = await payload.find({ collection: "pages", limit: 200, where: query });
+    const payload = await getPayload({ config })
+    const data = await payload.find({ collection: 'pages', limit: 200, where: query })
 
-    return NextResponse.json(data, { status: 200 });
+    return NextResponse.json(data, { status: 200 })
   } catch (error) {
-    return NextResponse.json({ error: "Failed to fetch data" }, { status: 500 });
+    return NextResponse.json({ error: 'Failed to fetch data' }, { status: 500 })
   }
 }
